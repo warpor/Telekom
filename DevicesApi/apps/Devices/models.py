41c736d233d2e4e85183975fe1e4e60e869b239c
warpor <sp_8@list.ru>
2023-10-22 19:47:48 +0500

    Create models

--- DevicesApi/apps/Devices/models.py
+++ DevicesApi/apps/Devices/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from models_manager import SoftDeleteModel
+
+
+class DeviceType(models.Model):
+    type_name = models.CharField()
+    serial_num_mask = models.CharField()
+
+
+class Device(SoftDeleteModel):
+    type_id = models.ForeignKey(DeviceType, on_delete=models.CASCADE)
+    serial_num = models.CharField()
+    note = models.TextField()
---
+++ DevicesApi/apps/Devices/models_manager.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class SoftDeleteManager(models.Manager):
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=False)
+
+
+class SoftDeleteModel(models.Model):
+    is_deleted = models.BooleanField(default=False)
+    objects = models.Manager()
+    undeleted_objects = SoftDeleteManager()
+
+    def soft_delete(self):
+        self.is_deleted = True
+        self.save()
+
+    def restore(self):
+        self.is_deleted = False
+        self.save()
+
+    class Meta:
+        abstract = True
