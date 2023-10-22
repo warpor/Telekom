241093983622a29ce0dc92197128e7c79214e114
warpor <sp_8@list.ru>
2023-10-22 19:55:03 +0500

    Add devices app in project

--- DevicesApi/apps/devices/models.py
+++ DevicesApi/apps/devices/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
-from models_manager import SoftDeleteModel
+from .models_manager import SoftDeleteModel
 
 
 class DeviceType(models.Model):
-    type_name = models.CharField()
-    serial_num_mask = models.CharField()
+    type_name = models.TextField()
+    serial_num_mask = models.TextField()
 
 
 class Device(SoftDeleteModel):
     type_id = models.ForeignKey(DeviceType, on_delete=models.CASCADE)
-    serial_num = models.CharField()
+    serial_num = models.TextField()
     note = models.TextField()
