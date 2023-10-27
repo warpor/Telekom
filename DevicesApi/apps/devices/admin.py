853a9978b13ea7fa473ca84c80904be3c2b1eaaa
warpor <sp_8@list.ru>
2023-10-27 18:31:11 +0500

    Remove model from admin panel

--- DevicesApi/apps/devices/admin.py
+++ DevicesApi/apps/devices/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import DeviceType, Device
+from .models import DeviceType
 
 # Register your models here.
 admin.site.register(DeviceType)
-admin.site.register(Device)
