17cfe6a119912e01c897f3b90f6830b89a7f9520
warpor <sp_8@list.ru>
2023-10-27 12:00:06 +0500

    Add validators

---
+++ DevicesApi/apps/devices/validators.py
@@ -0,0 +1,33 @@
+import re
+
+from rest_framework import serializers
+
+from .models import DeviceType
+
+regex_map = {
+    "N": r"\d",
+    "A": r"[A-Z]",
+    "a": r"[a-z]",
+    "X": r"[A-Z] | \d",
+    "Z": r"[_ - @]"
+}
+
+
+
+class SerialNumValidator:
+
+    def get_serial_mask(self, device_type: DeviceType) -> str:
+        return device_type.serial_num_mask
+
+    def __call__(self, attrs):
+        serial_mask = self.get_serial_mask(attrs["type_id"])
+        if not self.check_serial_num(serial_mask, attrs["serial_num"]):
+            raise serializers.ValidationError("Серийный номер не подходит под маску")
+
+    def check_serial_num(self, serial_mask, serial_num):
+        new_mask: list[str] = []
+        for each_num in serial_mask:
+            new_mask.append(regex_map[each_num])
+        if re.fullmatch("".join(new_mask), serial_num):
+            return True
+        return False
